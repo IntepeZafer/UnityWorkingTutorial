ea168542159f9004214449ec920a5b997c34b0f6
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-08-15 15:28:48 +0300

    commit

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,17 +1,23 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private float movementX;
     private float movementY;
     public float speed = 10f;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        winTextObject.SetActive(false);
+        SetCountText();
+        count = 0;
     }
     public void OnMove(InputValue movementValue)
     {
@@ -23,5 +29,22 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f , movementY);
         rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+        }
+        SetCountText();
+    }
+    void SetCountText() 
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 4)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 }
